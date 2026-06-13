3067376f7a729512fa29780b7d510d009692fd99
Anton Woody <antonwoody29@gmail.com>
2026-06-13 09:16:57 -0400

    Initial Commit

---
+++ AJ Finance/AJ_FinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AJ_FinanceApp.swift
+//  AJ Finance
+//
+//  Created by Anton Woody on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct AJ_FinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AJ Finance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AJ Finance
+//
+//  Created by Anton Woody on 6/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
